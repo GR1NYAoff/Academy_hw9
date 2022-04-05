9cfaf0074f5f2d89354e8b7ca156d6efc1627c62
= <=>
2022-04-05 17:20:06 +0300

    tasks implemented

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,29 @@
+﻿var hostBuilder = new HostBuilder()
+    .ConfigureAppConfiguration(builder =>
+    {
+        builder
+        .AddJsonFile("appsettings.json", false, true)
+        .AddEnvironmentVariables(); //use all environment variables
+    })
+    .ConfigureLogging(builder =>
+    {
+        builder
+        .SetMinimumLevel(LogLevel.Information) // from file
+        .AddJsonConsole(options =>
+        {
+            options.TimestampFormat = "hh:mm:ss";
+        });
+    })
+    .ConfigureServices(services =>
+    {
+        services.AddSingleton<Worker>();
+    });
+
+var host = hostBuilder.Build();
+
+var configuration = host.Services.GetRequiredService<IConfiguration>(); //01:08
+
+var logger = host.Services.GetRequiredService<ILogger<Program>>();
+logger.LogInformation("test log");
+
+await host.RunAsync();
